e2de40e6e92d622c95f92f6ab0d22e95079ec119
Bivonmoses <bivonmoses61@gmail.com>
2023-11-29 16:53:37 +0300

    deploy

---
+++ js/script.js
@@ -0,0 +1,69 @@
+let Navbar = document.querySelector('.navbar');
+let Fabars = document.querySelector('.fa-bars');
+
+Fabars.onclick = () =>{
+    Navbar.classList.toggle("active")
+};
+
+
+
+
+var swiper = new Swiper(".home-slid", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
+
+
+
+var swiper = new Swiper(".team-slider", {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 10,
+    breakpoints: {
+      "0": {
+        slidesPerView: 1,
+        autoplay:{
+            delay:700,
+            disableOnInteraction:false,
+        },
+      },
+      "768": {
+        slidesPerView: 2,
+          
+      },
+      "1020": {
+        slidesPerView: 3,
+          
+      },
+    },
+  });
+  function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+  function toggleReadMore(boxId) {
+    var content = document.getElementById(`read-more-content-${boxId}`);
+    var button = document.getElementById(`read-more-button-${boxId}`);
+
+    if (content.style.display === "none") {
+      content.style.display = "block";
+      button.innerHTML = "Read Less";
+    } else {
+      content.style.display = "none";
+      button.innerHTML = "Read More";
+    }
+  }
+  
